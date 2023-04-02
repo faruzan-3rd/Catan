9e952d153052c36e05a942c38c345345bdf4c052
wasabi3D <tryexceptfinally5@gmail.com>
2023-04-02 10:24:21 +0200

    Added forest, brick and mountain tiles

--- include/catan/constants.hpp
+++ include/catan/constants.hpp
@@ -9,19 +9,28 @@ namespace ctn{
     enum class Tile{
         DESERT,
         PLAIN,
-        WHEAT
+        WHEAT,
+        MOUNTAIN,
+        BRICK,
+        FOREST
     };
 
     static const ctn::Tile All_types[] = {
         ctn::Tile::DESERT,
         ctn::Tile::PLAIN,
-        ctn::Tile::WHEAT
+        ctn::Tile::WHEAT,
+        ctn::Tile::MOUNTAIN,
+        ctn::Tile::BRICK,
+        ctn::Tile::FOREST
     };
 
     static const std::map<ctn::Tile, std::string> enum2str = {
         {ctn::Tile::DESERT, "desert"},
         {ctn::Tile::PLAIN, "plain"},
-        {ctn::Tile::WHEAT, "wheat"}
+        {ctn::Tile::WHEAT, "wheat"},
+        {ctn::Tile::MOUNTAIN, "mountain"},
+        {ctn::Tile::BRICK, "brick"},
+        {ctn::Tile::FOREST, "forest"}
     };
 }
 
